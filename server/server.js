2c9c7361715a9340222d50000f506ed8cf13b42e
Eric Pineda <ericvpineda@berkeley.edu>
2023-11-04 20:17:57 -0700

    updated -- server index.js added local mongdb server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
@@ -9,40 +9,53 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Note: need to define due to es6 
+dotenv.config();
+
+// Note: need to define due to es6
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
+const PORT = process.env.PORT || 8001;
+let MONGODB_URL = process.env.MONGODB_PROD;
 
-dotenv.config()
+if (process.env.NODE_ENV === 'development') {
+    MONGODB_URL = process.env.MONGODB_DEV;
+}
+
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use(helmet());
-// Prevent other websites from using current website cross-origin 
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
-app.use(morgan("common"))
-app.use(bodyParser.json({limit: "30mb", extended: true}))
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+// Prevent other websites from using current website cross-origin
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 // Set directory of where keep assets (Note: Production version needs to be stored on cloud)
-app.use("/assests", express.static(path.join(__dirname, "public/assets")))
+app.use("/assests", express.static(path.join(__dirname, "public/assets")));
 
 // Local file storage for multi-media
 const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, "public/assets")
-    },
-    filename: (req, file, callback) => {
-        callback(null, file.originalname)
-    }
-})
+  destination: (req, file, callback) => {
+    callback(null, "public/assets");
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  },
+});
 
-const upload = multer({storage})
+const upload = multer({ storage });
+
+mongoose.connect(MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Database connected!"))
+.catch(() => console.log("Database connection failed."))
 
 // Test route
-app.get('/message', (req, res) => {
-    res.json({message: "Hello from server!"})
-})
+app.get("/message", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
 
-const port = 8000
-app.listen(port, () => console.log(`Server is running on port ${port}.`))
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}.`));
