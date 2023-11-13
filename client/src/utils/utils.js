c61dc7fcf45a50a7e62235b043f94faa0b2def45
Eric Pineda <ericvpineda@berkeley.edu>
2023-11-12 17:22:17 -0800

    updated -- client added getUser to utils

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -2,4 +2,16 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-export {classNames};
+const getUser = async ({userId, token, setuser}) => {
+    const response = await fetch(`http://localhost:8000/user/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    const user = await response.json();
+    if (response.ok) {
+      setuser(user);
+    }
+  };
+
+export {classNames, getUser};
