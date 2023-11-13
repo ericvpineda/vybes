fb58cc0d8d0360c4c7ba2d0f811b7339c1617e5c
Eric Pineda <ericvpineda@berkeley.edu>
2023-11-13 00:28:14 -0800

    updated -- client profile basic ui

--- client/src/routes/Profile.jsx
+++ client/src/routes/Profile.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import CreatePostWidget from "components/Widget/CreatePostWidget";
+import FeedWidget from "components/Widget/FeedWidget";
+import FriendsWidget from "components/Widget/FriendsWidget";
+import UserWidget from "components/Widget/UserWidget.jsx";
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div className="h-full w-full lg:flex block gap-4 lg:justify-center">
+      <div className="basis-[32%]">
+        <UserWidget />
+        <FriendsWidget/>
+      </div>
+      <div className="basis-[50%]">
+        <CreatePostWidget />
+        <FeedWidget isProfile={true}/>
+      </div>
+    </div>
   )
 }
