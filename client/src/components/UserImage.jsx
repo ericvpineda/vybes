aac659097ee91be6e363a0372d2b231cfa4671e2
Eric Pineda <ericvpineda@berkeley.edu>
2023-11-15 23:29:27 -0800

    updated -- client UserImage.jsx profile image edit implemented

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -1,13 +1,24 @@
 import { Box } from "@mui/material";
 import Face6Icon from "@mui/icons-material/Face6";
-import { HOST_BACKEND, classNames } from "utils/utils";
+import { HOST_BACKEND } from "utils/utils";
 
 export default function UserImage({ name, size = "60px" }) {
+  if (name) {
+    if (typeof name === "string") {
+      name = `${HOST_BACKEND}/assets/${name}`;
+    } else {
+      name = URL.createObjectURL(name);
+    }
+  }
   return (
-    <Box width={size} height={size}>
+    <Box
+      width={size}
+      height={size}
+      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+    >
       {name && name.length > 0 ? (
         <img
-          src={`${HOST_BACKEND}/assets/${name}`}
+          src={name}
           alt="User."
           className="object-cover rounded-[50%]"
           style={{ minHeight: size, minWidth: size }}
