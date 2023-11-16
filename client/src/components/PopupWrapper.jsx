9a2e93a574d6550c855e8372cba669bd64f75c53
Eric Pineda <ericvpineda@berkeley.edu>
2023-11-15 22:06:55 -0800

    added -- client PopupWrapper.jsx for popup modals

---
+++ client/src/components/PopupWrapper.jsx
@@ -0,0 +1,10 @@
+export default function PopupWrapper({ children, togglePopup }) {
+  return (
+    <div
+      className="bg-[#000000aa] dark:bg-darkBackground-0  fixed top-0 left-0 bottom-0 right-0 flex justify-center items-center mx-auto z-20 m-0 p-0"
+      onClick={() => togglePopup()}
+    >
+      <div onClick={(e) => e.stopPropagation()}>{children}</div>
+    </div>
+  );
+}
