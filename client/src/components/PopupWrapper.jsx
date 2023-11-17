f65f124ac90becf4b86e037fabc59b786ca2591b
Eric Pineda <ericvpineda@berkeley.edu>
2023-11-16 20:36:10 -0800

    updated -- client PopupWrapper darkmode ui

--- client/src/components/PopupWrapper.jsx
+++ client/src/components/PopupWrapper.jsx
@@ -1,7 +1,7 @@
 export default function PopupWrapper({ children, togglePopup }) {
   return (
     <div
-      className="bg-[#000000aa] dark:bg-darkBackground-0  fixed top-0 left-0 bottom-0 right-0 flex justify-center items-center mx-auto z-20 m-0 p-0"
+      className="bg-[#000000aa] fixed top-0 left-0 bottom-0 right-0 flex justify-center items-center mx-auto z-20 m-0 p-0"
       onClick={() => togglePopup()}
     >
       <div onClick={(e) => e.stopPropagation()}>{children}</div>
